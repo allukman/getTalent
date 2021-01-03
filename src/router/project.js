7a3399582dab01d91a4c5b61b7fd4306217f7b00
allukman <allukman1802@gmail.com>
2021-01-03 22:56:19 +0700

    add file

--- src/router/project.js
+++ src/router/project.js
@@ -1,11 +1,11 @@
 const { Router } = require('express')
 const { createProject, getProjectById, getProjectByComId, updateProject, deleteProject } = require('../controllers/project')
-const { authorizationCompany } = require('../middleware/auth')
+const { authorizationCompany, authorizationAll } = require('../middleware/auth')
 const uploadImage = require('../middleware/multer')
 const router = Router()
 
 router.post('/', authorizationCompany, uploadImage, createProject)
-router.get('/:pjId', authorizationCompany, getProjectById)
+router.get('/:pjId', authorizationAll, getProjectById)
 router.get('/company/:comId', authorizationCompany, getProjectByComId)
 router.put('/:pjId', authorizationCompany, uploadImage, updateProject)
 router.delete('/:pjId', authorizationCompany, deleteProject)

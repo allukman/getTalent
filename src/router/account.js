f0b03ac816ac2df29c1a71ffe72030bba75bc825
allukman <allukman1802@gmail.com>
2021-01-14 21:02:08 +0700

    add file

--- src/router/account.js
+++ src/router/account.js
@@ -3,8 +3,8 @@ const { getAccountById, getAccount, createAccount, updateAccount, deleteAccount,
 const { authorizationAdmin, authorizationAll } = require('../middleware/auth')
 const router = Router()
 
-router.get('/:accountId', authorizationAdmin, getAccountById)
-router.get('/', authorizationAdmin, getAccount)
+router.get('/:accountId', authorizationAll, getAccountById)
+router.get('/', authorizationAll, getAccount)
 router.post('/register', createAccount)
 router.put('/:accountId', authorizationAll, updateAccount)
 router.delete('/:accountId', authorizationAdmin, deleteAccount)

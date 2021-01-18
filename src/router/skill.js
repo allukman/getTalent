ea0c08f44f32e6bfffd97be0f1ee8d127dc9f495
allukman <allukman1802@gmail.com>
2021-01-18 10:55:20 +0700

    add file

--- src/router/skill.js
+++ src/router/skill.js
@@ -1,11 +1,11 @@
 const { Router } = require('express')
 const { createSkill, getSkillById, getSkillByEngId, updateSkill, deleteSkill } = require('../controllers/skill')
-const { authorizationEngineer } = require('../middleware/auth')
+const { authorizationEngineer, authorizationAll } = require('../middleware/auth')
 const router = Router()
 
 router.post('/', authorizationEngineer, createSkill)
 router.get('/:skId', authorizationEngineer, getSkillById)
-router.get('/engineer/:engineerId', authorizationEngineer, getSkillByEngId)
+router.get('/engineer/:engineerId', authorizationAll, getSkillByEngId)
 router.put('/:skId', authorizationEngineer, updateSkill)
 router.delete('/:skId', authorizationEngineer, deleteSkill)
 

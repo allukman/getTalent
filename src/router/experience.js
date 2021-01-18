173863d3a93f7c84de7c13b61fac5385e75a2994
allukman <allukman1802@gmail.com>
2021-01-18 11:51:47 +0700

    add file

--- src/router/experience.js
+++ src/router/experience.js
@@ -1,12 +1,12 @@
 const { Router } = require('express')
 const { createExperience, getExperienceById, getExperienceByEngId, updateExperience, deleteExperience } = require('../controllers/experience')
-const { authorizationEngineer } = require('../middleware/auth')
+const { authorizationEngineer, authorizationAll } = require('../middleware/auth')
 const uploadImage = require('../middleware/multer')
 const router = Router()
 
 router.post('/', authorizationEngineer, uploadImage, createExperience)
 router.get('/:xpId', authorizationEngineer, getExperienceById)
-router.get('/engineer/:engineerId', authorizationEngineer, getExperienceByEngId)
+router.get('/engineer/:engineerId', authorizationAll, getExperienceByEngId)
 router.put('/:xpId', authorizationEngineer, uploadImage, updateExperience)
 router.delete('/:xpId', authorizationEngineer, deleteExperience)
 
--- src/router/portofolio.js
+++ src/router/portofolio.js
@@ -1,12 +1,12 @@
 const { Router } = require('express')
 const { createPortofolio, getPortofolioById, getPortofolioByEngId, updatePortofolio, deletePortofolio } = require('../controllers/portofolio')
-const { authorizationEngineer } = require('../middleware/auth')
+const { authorizationEngineer, authorizationAll } = require('../middleware/auth')
 const uploadImage = require('../middleware/multer')
 const router = Router()
 
 router.post('/', authorizationEngineer, uploadImage, createPortofolio)
 router.get('/:prId', authorizationEngineer, getPortofolioById)
-router.get('/engineer/:engineerId', authorizationEngineer, getPortofolioByEngId)
+router.get('/engineer/:engineerId', authorizationAll, getPortofolioByEngId)
 router.put('/:prId', authorizationEngineer, uploadImage, updatePortofolio)
 router.delete('/:prId', authorizationEngineer, deletePortofolio)
 
